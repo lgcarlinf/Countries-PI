dcfb5ca9e47fdd3e82a107beef4847ea8be200bf
luiggylml <luiggylml@gmail.com>
2021-12-16 00:09:28 -0500

    deppp

--- client/src/index.js
+++ client/src/index.js
@@ -1,24 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {Provider} from 'react-redux';
-import {store} from './store';
-import dotenv from 'dotenv';
-import axios from 'axios';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import { store } from "./store";
+import dotenv from "dotenv";
+import axios from "axios";
 dotenv.config();
 /*     hola */
-axios.defaults.baseURL = process.env.REACT_APP_API || 'http://localhost:3001';
-process.env.REACT_APP_CI = 'npm run build'
+let api = process.env.REACT_APP_API || "http://localhost:3001";
+axios.defaults.baseURL = api;
+console.log(api);
+
+process.env.REACT_APP_CI = "npm run build";
 
 ReactDOM.render(
   <Provider store={store}>
-  <React.StrictMode>
-  <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
