3ee025ec388768ddcb67b5b8051ac8a50d52d32b
luiggy <luiggylml@gmail.com>
2022-12-06 15:53:19 -0500

    test2

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./LandingPage.css";
-import video from "https://countries-g56m8unth-lgcarlinf.vercel.app/";
 import { Loading } from "../Loading/Loading";
 
 export const LandingPage = () => {
   const [init, setInit] = useState(false);
+
+  const video =
+    "https://res.cloudinary.com/dhtyfvv9i/video/upload/v1670359582/Time_and_Again_15_Countries_In_4_Minutes_2014_Time_Lapse_online_video_cutter_com_wogc2a.mp4";
 
   useEffect(() => {
     if (video) {
