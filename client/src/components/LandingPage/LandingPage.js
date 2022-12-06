3e6cf6ba59824ffcf1403cd7fa9eb55e11f9bd07
luiggy <luiggylml@gmail.com>
2022-12-06 15:44:12 -0500

    test2

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, Suspense, lazy } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import "./LandingPage.css";
 import video from "../../assets/Time and Again   15 Countries In 4 Minutes 2014 Time Lapse online video cutter com.mp4";
@@ -6,41 +6,30 @@ import { Loading } from "../Loading/Loading";
 
 export const LandingPage = () => {
   const [init, setInit] = useState(false);
-  /* 
+
   useEffect(() => {
-    const timer = setTimeout(() => {
+    if (video) {
       setInit(true);
-    }, 3000);
-    return () => {
-      clearTimeout(timer);
-    };
-  }, []);
- */
+    }
+  }, [video]);
+
   return (
     <>
-      <div className="containerLanding">
-        <Suspense
-          fallback={
-            <div className="container-general" style={{ alignItems: "center" }}>
-              <Loading />
-            </div>
-          }
-        >
-          <video
-            className="video "
-            poster="../../assets/mapa.jpeg"
-            autoPlay
-            loop
-            muted
-          >
+      {init ? (
+        <div className="containerLanding">
+          <video className="video " autoPlay loop muted>
             <source src={video} type="video/mp4" />
           </video>
-        </Suspense>
-        <h1>Countries PI</h1>
-        <Link className="link" to="/countries">
-          Ingresa Aqui
-        </Link>
-      </div>
+          <h1>Countries PI</h1>
+          <Link className="link" to="/countries">
+            Ingresa Aqui
+          </Link>
+        </div>
+      ) : (
+        <div className="container-general" style={{ alignItems: "center" }}>
+          <Loading />
+        </div>
+      )}
     </>
   );
 };
